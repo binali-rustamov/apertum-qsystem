e3206f1ff4e228d0a631e6155cbd9e95ab6125fd
evgeniy.egorov <evgeniy.egorov@localhost>
2010-10-13 12:53:39 +0400

    Add start for tablo redactor. Update help image.

---
+++ installation/resource/txt/StartTabloRedactor.bat
@@ -0,0 +1,2 @@
+start javaw -cp dist/QSystem.jar ru.apertum.qsystem.client.TabloRedactor config/clientboard.xml
+
---
+++ installation/resource/txt/starttabloredactor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp dist/QSystem.jar ru.apertum.qsystem.client.TabloRedactor config/clientboard.xml
